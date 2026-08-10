105595d930d9fdcb698af1225e0bf08f3a09b20d
Ярек <yarohatsune@gmail.com>
2026-08-10 23:18:15 +0500

    test tsc, npm commands

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 let num: number = 5;
-console.log(`Variable: ${num}`);
+
+type valid = string | number;
+// console.log(num);
+
+interface User{
+    nickname: string,
+    email: valid,
+    password: valid
+}
+
+console.log(num * 5)
