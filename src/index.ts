268ae445af6a1646161714ac4b48827dc0241f46
Ярек <yarohatsune@gmail.com>
2026-08-29 23:03:11 +0500

    add own module

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,18 @@ let num: number = 5;
 type valid = string | number;
 console.log(`Variable: ${num}`);
 
-interface User{
-    nickname: string,
-    email: valid,
-    password: valid
-}
+// interface User{
+//     nickname: string,
+//     email: valid,
+//     password: valid
+// }
 
-console.log(num * 5)
+// console.log(num * 5)
+import os from 'os';
+import math from './testMath.js';
+
+let res2: number = math.plus(2,3);
+console.log(res2)
+
+let res: any = os.platform()
+console.log(res)
---
+++ src/testMath.ts
@@ -0,0 +1,13 @@
+const plus = (a: number, b: number) => {
+    return a + b
+}
+const min = (a: number, b: number) => {
+    return a - b
+}
+const x = (a: number, b: number) => {
+    return a * b
+}
+
+export default {
+    plus, min, x
+}
