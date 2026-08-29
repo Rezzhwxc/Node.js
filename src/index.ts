56eda165a88500db81a9661e24e3344ecdb95bba
Ярек <yarohatsune@gmail.com>
2026-08-30 00:44:37 +0500

    test module

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,8 @@ import os from 'os';
 import math from './testMath.js';
 
 let res2: number = math.plus(2,3);
-console.log(res2)
+let res3: number = math.x(100, 3);
+console.log(`Ans1 - ${res2}, Ans2 - ${res3}`)
 
 let res: any = os.platform()
-console.log(res)
+console.log(`Your OS: ${res}`)
